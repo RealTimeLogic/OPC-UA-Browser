226c1ac3d6a1abeafc7184a17ce2b404746cf999
Alexander Rykovanov <alexander@realtimelogic.com>
2023-06-02 22:31:55 +0300

    Show current time on server

---
+++ src/components/ServerTime.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+const serverTime = defineProps(['serverTime'])
+
+</script>
+
+<template>
+    <div>
+        <span style="color: white">Server time: {{ serverTime.serverTime }}</span>
+    </div>
+</template>
